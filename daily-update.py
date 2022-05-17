9a0a08e1d39777431d4f29076d5c13dd0f929d22
Scott Veirs <scott@beamreach.org>
2022-05-17 15:29:21 -0700

    Refactor repo with Val on phone

--- daily-update.py
+++ daily-update.py
@@ -29,6 +29,10 @@ def thumbnails(savedir, imgfile, scalefactor):
 date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
 print(f"filename_{date}")
 
+# Move any older files from /data/latest-LO to /data/archive-LO
+
+
+
 # Use Requests to download the latest model movie from the LiveOcean site
 # Store the mp4 in /data/latest-LO
 cwd = os.getcwd()
@@ -36,20 +40,22 @@ currentfile = "https://faculty.washington.edu/pmacc/LO/Figs_active_forecast/P1_P
 print("Current working directory is", cwd)
 print("ls is", os.listdir())
 r = requests.get(currentfile, allow_redirects=True)
-open('{}/data/latest-LO/P1_PS_speed_top.mp4'.format(cwd), 'wb').write(r.content)
+open('{}/docs/data/latest-LO/P1_PS_speed_top.mp4'.format(cwd), 'wb').write(r.content)
 
 # Use ffmpeg to extract frames from each hour as individual image files
+# and store the output in /data/latest-LO
 
-os.system('ffmpeg -i {}/data/latest-LO/P1_PS_speed_top.mp4 -vf fps=8 {}/data/latest-LO/img/plot_%04d.png'.format(cwd, cwd))
+os.system('ffmpeg -i {}/docs/data/latest-LO/P1_PS_speed_top.mp4 -vf fps=8 {}/doc/data/latest-LO/img/plot_%04d.png'.format(cwd, cwd))
 scalefactor = 3    # scalefactor = 2 give a more 'readable' thumbnail
-imgDir = "{}/data/latest-LO/img/".format(cwd)
+imgDir = "{}/docs/data/latest-LO/img/".format(cwd)
 print(imgDir)
 for file in os.listdir(imgDir):
     print(file)
-    thumbnails("{}/data/latest-LO/thumbs/".format(cwd), "{}/data/latest-LO/img/".format(cwd) + file, 3)
+    thumbnails("{}/docs/data/latest-LO/thumbs/".format(cwd), "{}/docs/data/latest-LO/img/".format(cwd) + file, 3)
 
-# Store the output in /data/latest-LO
+# Delete source animation file (mp4)
+# os.system('rm {}/docs/data/latest-LO/P1_PS_speed_top.mp4'.format(cwd, cwd))
+os.remove('{}/docs/data/latest-LO/P1_PS_speed_top.mp4'.format(cwd, cwd))   
 
-# Move any older files from /data/latest-LO to /data/archive-LO
 
 
